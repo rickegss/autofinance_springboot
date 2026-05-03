a5e7ff6dd863ee30f6a3a7c276978d0bb2ff07ef
rickegss <ricksilvagomes1@gmail.com>
2026-05-03 12:13:53 -0300

    test: add Transaction Creating test

--- src/test/java/com/rickegss/autofinance/service/TransactionServiceImplTest.java
+++ src/test/java/com/rickegss/autofinance/service/TransactionServiceImplTest.java
@@ -1,11 +1,13 @@
 package com.rickegss.autofinance.service;
 
 import com.rickegss.autofinance.dto.TransactionDTO;
+import com.rickegss.autofinance.entity.Transaction;
 import com.rickegss.autofinance.entity.TransactionType;
 import com.rickegss.autofinance.entity.User;
 import com.rickegss.autofinance.repository.TransactionRepository;
 import com.rickegss.autofinance.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,6 +15,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceImplTest {
@@ -46,6 +53,36 @@ class TransactionServiceImplTest {
         );
     }
 
-    // @Test -> void create_shouldCreateTransactionSuccessfully() ...
+    @Test
+    void create_shouldCreateTransactionSuccessfully() {
+        // Arrange
+        when(userRepository.findByEmail("user@test.com"))
+                .thenReturn(Optional.of(user));
+
+        Transaction savedTransaction = Transaction.builder()
+                .id(1L)
+                .description(dto.description())
+                .amount(dto.amount())
+                .type(dto.type())
+                .category(dto.category())
+                .date(dto.date())
+                .user(user)
+                .build();
+
+        when(transactionRepository.save(any(Transaction.class)))
+                .thenReturn(savedTransaction);
+
+        // Act
+        Transaction result = transactionService.create("user@test.com", dto);
+
+        //Assert
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getAmount()).isEqualByComparingTo(BigDecimal.valueOf(1200.00));
+        assertThat(result.getType()).isEqualTo(TransactionType.DESPESA);
+        assertThat(result.getUser()).isEqualTo(user);
+
+        verify(transactionRepository, times(1)).save(any(Transaction.class));
+
+    }
 
 }
