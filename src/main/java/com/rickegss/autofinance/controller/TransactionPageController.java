e8deb2cf464616e56c20f7fdbdb54f4743e587d4
Ricardo Gomes <ricksilvagomes1@gmail.com>
2026-05-06 16:45:45 +0000

    feat: add transactions page

---
+++ src/main/java/com/rickegss/autofinance/controller/TransactionPageController.java
@@ -0,0 +1,13 @@
+package com.rickegss.autofinance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TransactionPageController {
+
+    @GetMapping("/transactions")
+    public String getTransactionsPage() {
+        return "transactions";
+    }
+}
