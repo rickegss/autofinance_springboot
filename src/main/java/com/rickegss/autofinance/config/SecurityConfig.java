1515f36d3069a7264188e82ca345105536b9fa59
Ricardo Gomes <ricksilvagomes1@gmail.com>
2026-05-20 11:16:01 -0300

    Allow access to root path in SecurityConfig

--- src/main/java/com/rickegss/autofinance/config/SecurityConfig.java
+++ src/main/java/com/rickegss/autofinance/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register", "/css/**", "/js/**", "/images/**", "/svg/**", "/assets/**").permitAll()
+                        .requestMatchers("/", "/login", "/register", "/css/**", "/js/**", "/images/**", "/svg/**", "/assets/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
