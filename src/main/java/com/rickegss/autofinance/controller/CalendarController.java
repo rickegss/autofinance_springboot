ff9e8bcd43d2f95ae439ec2f2164803576bb909d
Ricardo Gomes <ricksilvagomes1@gmail.com>
2026-05-20 11:35:07 -0300

    feat: controller de calendário

---
+++ src/main/java/com/rickegss/autofinance/controller/CalendarController.java
@@ -0,0 +1,12 @@
+package com.rickegss.autofinance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalendarController {
+    @GetMapping("/calendar")
+    public String calendar() {
+        return "calendar";
+    }
+}
