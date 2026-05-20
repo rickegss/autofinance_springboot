3754229aa7e3b164378054da8ef6c52662214cc8
rickegss <ricksilvagomes1@gmail.com>
2026-05-19 22:35:40 -0300

    fix: recuperação de códigos de relatorio

--- src/main/java/com/rickegss/autofinance/dto/CycleDataDTO.java
+++ src/main/java/com/rickegss/autofinance/dto/CycleDataDTO.java
@@ -1,4 +1,19 @@
 package com.rickegss.autofinance.dto;
 
-public record CycleDataDTO() {
-}
+import com.rickegss.autofinance.entity.Transaction;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public record CycleDataDTO(
+        LocalDate cycleStart,
+        LocalDate cycleEnd,
+        BigDecimal totalIncome,
+        BigDecimal totalExpenses,
+        BigDecimal savings,
+        BigDecimal savingsPercentage,
+        BigDecimal expensesPercentage,
+        Map<String, BigDecimal> expensesByCategory,
+        List<Transaction> transactions
+) {}
