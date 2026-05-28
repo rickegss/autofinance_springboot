a5ef4863203ed172ca4dca210b21096dd0b1ab96
Ricardo Gomes <ricksilvagomes1@gmail.com>
2026-05-28 10:17:25 -0300

    Add Jackson configuration for ObjectMapper

---
+++ src/main/java/com/rickegss/autofinance/config/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.rickegss.autofinance.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.modules(new JavaTimeModule());
+        builder.timeZone(TimeZone.getTimeZone("America/Sao_Paulo")); 
+        return builder.build();
+    }
+}
