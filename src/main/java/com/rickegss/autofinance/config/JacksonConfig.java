92a7298caf8d8175211e1b1cb0887e0dd06cf00d
Ricardo Gomes <ricksilvagomes1@gmail.com>
2026-06-15 10:56:02 -0300

    Configure ObjectMapper to not write dates as timestamps
    Disable serialization of dates as timestamps in ObjectMapper.

--- src/main/java/com/rickegss/autofinance/config/JacksonConfig.java
+++ src/main/java/com/rickegss/autofinance/config/JacksonConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.time.ZoneId;
 import java.util.TimeZone;
@@ -12,11 +13,12 @@ import java.util.TimeZone;
 @Configuration
 public class JacksonConfig {
 
-    @Bean
+   @Bean
     public ObjectMapper objectMapper() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         builder.modules(new JavaTimeModule());
-        builder.timeZone(TimeZone.getTimeZone("America/Sao_Paulo")); 
+        builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        builder.timeZone(TimeZone.getTimeZone("America/Sao_Paulo"));
         return builder.build();
     }
 }
